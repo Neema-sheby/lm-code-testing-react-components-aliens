2da5e24dc3df8bf961c2b9ad2281b2db2969cc8a
neema <neemadas@gmail.com>
2023-02-03 18:14:23 +0000

    test: Add test cases for option component

--- src/components/Form/Options/OptionsData.tsx
+++ src/components/Form/Options/OptionsData.tsx
@@ -1,4 +1,9 @@
-export const OPTIONS = [
+interface Option {
+  label: string;
+  value: string;
+}
+
+export const OPTIONS: Array<Option> = [
   { label: "Choose the correct answer", value: "" },
   { label: "4", value: "4" },
   { label: "Not 4", value: "Not 4" },
---
+++ src/components/Form/Options/W12MOption.test.tsx
@@ -0,0 +1,20 @@
+import { screen, render } from "@testing-library/react";
+import W12MOption from "./W12MOption";
+
+///-----Test 1 -----/////////////////////////////////////////////////////////////
+
+it("renders the option field", () => {
+  render(<W12MOption value="Not 4">Not 4</W12MOption>);
+
+  const optionField = screen.getByRole("option");
+
+  expect(optionField).toBeInTheDocument();
+});
+
+it("The props are working well", () => {
+  render(<W12MOption value="Not 4">Not 4</W12MOption>);
+
+  const optionField = screen.getByRole("option");
+
+  expect(optionField).toHaveValue("Not 4");
+});

b71aea3e95dc46a420520618dbb9b8e321f4cbe4
neema <neemadas@gmail.com>
2023-02-05 06:16:18 +0000

    Refactor: Add error messages to a separate file and export them.

--- src/components/ErrorHandling/ErrorMessage.tsx
+++ src/components/ErrorHandling/ErrorMessage.tsx
@@ -1,3 +1,62 @@
+///----- Error variables-----/////////////////////////////////////////////////////////////
+
+export interface ErrorLog {
+  errorSpecies: string;
+  errorPlanet: string;
+  errorNumOfBeings: string;
+  errorSelect: string;
+  errorTextArea: string;
+}
+
+export const initialErrorLog: ErrorLog = {
+  errorSpecies: "",
+  errorPlanet: "",
+  errorNumOfBeings: "",
+  errorSelect: "",
+  errorTextArea: "",
+};
+
+///----- Error messages-----/////////////////////////////////////////////////////////////
+
+// Error messages for species input field
+export const errMsgSpecies = {
+  errEmpty: "⛔️ Error : Field is empty ! Please enter a valid species name !",
+  errCharCount:
+    "⛔️ Error : Species name must be between 3 and 23 characters !",
+  errValidString: "⛔️ Error : Please enter a valid string for species name !",
+};
+
+// Error messages for planet input field
+/////////////////////////////////////////////////////////////////////////////////
+
+export const errMsgPlanet = {
+  errEmpty: "⛔️ Error : Field is empty ! Please enter a valid planet name !",
+  errCharCount: "⛔️ Error : Planet name must be between 2 and 49 characters !",
+  errValidString: "⛔️ Error : Please enter a valid string for planet name !",
+};
+
+// Error messages for number of beings input field
+export const errMsgNumOfBeings = {
+  errEmpty:
+    "⛔️ Error : Field is empty ! Please enter a valid Number of beings !",
+  errNum: "⛔️ Error : Number of beings must be at least 1,000,000,000 !",
+  errValidNumber: "⛔️ Error : Please enter a valid number of beings !",
+};
+
+// Error messages for select input field
+export const errMsgSelect = {
+  errNotSelected:
+    "⛔️ Error : Not selected ! Please select an answer from the option !",
+  errInvalidAnswer: "⛔️ Error : You selected the wrong answer !",
+};
+
+// Error messages for textarea input field
+export const errMsgTextArea = {
+  errEmpty: "Error: Field is empty ! Please enter a valid reason for sparing !",
+  errCharCount:
+    "⛔️ Error : Reason for sparing must be between 17 and 153 characters !",
+};
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 interface ErrorMessageProp {
@@ -7,42 +66,9 @@ interface ErrorMessageProp {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 export const ErrorMessage: React.FC<ErrorMessageProp> = ({ errorMessage }) => {
-  return <div>{errorMessage}</div>;
-};
-
-///----- Error messages-----/////////////////////////////////////////////////////////////
-
-// Error messages for species input field
-export const errMsgSpecies = {
-  errEmpty: "Error: Field is empty ! Please enter a valid species name !",
-  errCharCount: "Error: Species name must be between 3 and 23 characters !",
-  errValidString: "Error: Please enter a valid string for species name",
-};
-
-// Error messages for planet input field
-export const errMsgPlanet = {
-  errEmpty: "Error: Field is empty ! Please enter a valid planet name !",
-  errCharCount: "Error: Planet name must be between 2 and 49 characters !",
-  errValidString: "Error: Please enter a valid string for planet name !",
-};
-
-// Error messages for number of beings input field
-export const errMsgNumOfBeings = {
-  errEmpty: "Error: Field is empty ! Please enter a valid Number of beings !",
-  errNum: "Error: Number of beings must be at least 1,000,000,000 !",
-  errValidNumber: "Error: Please enter a valid number of beings !",
-};
-
-// Error messages for select input field
-export const errMsgSelect = {
-  errNotSelected:
-    "Error: Not selected ! Please select an answer from the option !",
-  errInvalidAnswer: "Error: You selected the wrong answer! You failed !",
-};
-
-// Error messages for textarea input field
-export const errMsgTextArea = {
-  errEmpty: "Error: Field is empty ! Please enter a valid reason for sparing !",
-  errCharCount:
-    "Error: Reason for sparing must be between 17 and 153 characters !",
+  return (
+    <div role="log" className="form__error">
+      {errorMessage}
+    </div>
+  );
 };
